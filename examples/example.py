24187eabf531a1d5612333f8741b3c3846917ef1
Joerg Simon <j_simon@mac.com>
2017-05-21 17:02:48 +0200

    work till binary thresholding done so far

--- examples/example.py
+++ examples/example.py
@@ -1,3 +1,6 @@
+import numpy as np
+import cv2
+
 def warper(img, src, dst):
 
     # Compute and apply perpective transform
@@ -6,3 +9,37 @@ def warper(img, src, dst):
     warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST)  # keep same size as input image
 
     return warped
+
+def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
+    sobel = cv2.Sobel(img, cv2.CV_64F, int(orient=='x'), int(orient=='y'), ksize=sobel_kernel)
+    abs_sobel = np.absolute(sobel)
+    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+    binary_output = np.zeros_like(scaled_sobel)
+    binary_output[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+    return binary_output
+
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    mag = np.sqrt(sobelx**2 + sobely**2)
+    mag = np.uint8(255*mag/np.max(mag))
+    binary_output = np.zeros_like(mag)
+    binary_output[(mag >= mag_thresh[0]) & (mag <= mag_thresh[1])] = 1
+    return binary_output
+
+def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
+    img = np.copy(img)
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    abs_sobelx = np.absolute(sobelx)
+    abs_sobely = np.absolute(sobely)
+    grad_dir = np.arctan2(abs_sobely, abs_sobelx)
+    binary_output = np.zeros_like(grad_dir)
+    binary_output[(grad_dir >= thresh[0]) & (grad_dir <= thresh[1])] = 1
+    return binary_output
+
+def hls_select(hls_img, thresh=(0, 255)):
+    S = hls[:,:,2]
+    binary_output = np.zeros_like(S)
+    binary_output[(S > thresh[0]) & (S <= thresh[1])] = 1
+    return binary_output
